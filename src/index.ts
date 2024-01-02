9945304ac6ce4c755f476ea672878ab0c4fe0c45
Radha743 <worldofnara777@gmail.com>
2024-01-02 12:20:52 +0530

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { Elysia } from "elysia";
+import * as Routes from "./route";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.get("/", Routes.home);
+app.get("/healthcheck", Routes.healthcheck);
+app.get("/hello/:name", Routes.hello);
+app.get("/users", Routes.users);
+app.get("/users/:id", Routes.usersWithId);
+
+app.listen(3000, () => {
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  );
+});
